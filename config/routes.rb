bcf3ca796b410cef930960bbb172da8848284412
Manfredi <manfbenvenuti@gmail.com>
2015-01-30 12:16:42 +0100

    Coming soon take 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+  get 'pivot' => 'pages#pivot'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
